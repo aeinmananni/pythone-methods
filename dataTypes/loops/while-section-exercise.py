81289d2181a334aae7a7a616af909b49607581fa
aein <mananniaein1234@gmail.com>
2025-09-20 21:08:18 +0330

    add

---
+++ dataTypes/loops/while-section-exercise.py
@@ -0,0 +1,10 @@
+n = 50
+
+while n != 1:
+    print(n)
+    if n % 2 == 0:
+        n //= 2
+    else:
+        n = 3 * n + 1
+
+print(n)  
---
+++ dataTypes/loops/while.py
@@ -0,0 +1,13 @@
+print("Wellcom To While")
+
+i = 0
+while i <= 5:
+    print(f"i is ${i}")
+    i += 1
+    # output => 
+    #         i is $0
+    #         i is $1
+    #         i is $2
+    #         i is $3
+    #         i is $4
+    #         i is $5    
