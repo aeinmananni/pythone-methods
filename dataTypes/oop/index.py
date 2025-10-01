29feeba08108db179fc99c4345dbcfee0f50de64
aein <mananniaein1234@gmail.com>
2025-10-01 21:28:49 +0330

    add new Change

--- dataTypes/oop/index.py
+++ dataTypes/oop/index.py
@@ -22,3 +22,23 @@ class Human():
 human1 = Human()
 
 print(f">> {human1.full_name()}")
+
+
+print("--------------------------")
+
+class Car():
+    def __init__(self,color:str,price:int,brand:str):
+        self.color = color
+        self.price = price
+        self.brand = brand
+
+    def Message(self):
+        return f"my Car Color {self.color} and Price {self.price} => Bard {self.brand}"    
+    
+
+
+Car1:Car = Car(color="Red",price=43210000,brand="BMW")    
+Car2:Car = Car(color="Blue",price=3200098,brand="Purch")    
+
+print("Car1 : ", Car1.Message())
+print("Car2 : ", Car2.Message())
