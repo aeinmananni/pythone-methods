ee96818d702a9bff8099a6ac8f3a4b97df9dac4d
aeinmananni <mananniaein1234@gmail.com>
2025-10-11 07:42:18 +0330

    ewrwerwer

---
+++ dataTypes/tamrin-loops/while.py
@@ -0,0 +1,25 @@
+print("While ....")
+
+i: int = 0
+
+while i < 5:
+    print("Hello World : ")
+    i += 1
+
+print("-------------------")
+
+count: int = 4
+while count <= 10:
+    print("Number is : ", count)
+    count += 1
+
+
+print("------------------")
+
+password: str = ""
+
+while password != "1234":
+    password = input("رمز ورود خود را وارد کنید : ")
+
+
+print("دسترسی مجاز : ✅")
